09b7986b2dff18f720107e6891d70045bb590663
Sam Ayler <sayler8701@gmail.com>
2021-02-08 09:21:47 -0800

    database model for workouts

---
+++ models/workout.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+	exercises: [
+		{
+			type: {
+				type: String,
+				trim: true,
+			},
+			name: {
+				type: String,
+				trim: true,
+			},
+			duration: {
+				type: Number,
+			},
+			weight: {
+				type: Number,
+			},
+			reps: {
+				type: Number,
+			},
+			sets: {
+				type: Number,
+			},
+		},
+	],
+	day: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = mongoose.model("Workout", workoutSchema);
