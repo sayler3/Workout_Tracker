7a1f593f2aa2b1698e543131b07d2d2614563d8a
Sam Ayler <sayler8701@gmail.com>
2021-02-08 13:22:36 -0800

    adding route to find all workouts from database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,21 @@
 const Workout = require("../models/workout");
 
-module.exports = function (app) {};
+module.exports = (app) => {
+	//find all workouts
+	app.get("/api/workouts", (req, res) => {
+		Workout.find()
+			.then((found) => {
+				res.json(found);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	});
+
+	//create a workout
+	// app.post("/api/workouts", (req, res) => {
+	// 	Workout.create({
+	// 		date: Date.now(),
+	// 	});
+	// });
+};
