505e0b72fd85e2f6eae7c11eabb52f570efd9b51
Sam Ayler <sayler8701@gmail.com>
2021-02-08 11:30:43 -0800

    setting up connection to database

---
+++ routes/apiRoutes.js
@@ -0,0 +1,3 @@
+const Workout = require("../models/workout");
+
+module.exports = function (app) {};
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Adding dependencies
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const logger = require("morgan");
 
 const app = express();
@@ -10,13 +9,23 @@ app.use(logger("dev"));
 // setting port the deploy and localhost
 const PORT = process.env.PORT || 3000;
 
+// setting up database connection
+mongoose
+	.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.catch((error) => handleError(error));
+
 // setting up middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// bringing in routes
 require("./routes/clientRoutes")(app);
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, function () {
 	console.log(`Listening on http://localhost:${PORT}`);
