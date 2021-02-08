8f19331ef8ae245a974063281172f6901873e482
Sam Ayler <sayler8701@gmail.com>
2021-02-08 08:59:19 -0800

    setting up intial server

---
+++ server.js
@@ -0,0 +1,20 @@
+// Adding dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const logger = require("morgan");
+
+const app = express();
+app.use(logger("dev"));
+
+// setting port the deploy and localhost
+const PORT = process.env.PORT || 3000;
+
+// setting up middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function () {
+	console.log(`Listening on http://localhost:${PORT}`);
+});
