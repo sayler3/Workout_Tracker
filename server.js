ce4e037847c2742a26bf54c913ca53b23756265f
Sam Ayler <sayler8701@gmail.com>
2021-02-09 11:24:01 -0800

    updating for deployment

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ mongoose
 	.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 		useNewUrlParser: true,
 		useFindAndModify: false,
+		useCreateIndex: true,
 		useUnifiedTopology: true,
 	})
 	.catch((error) => handleError(error));
