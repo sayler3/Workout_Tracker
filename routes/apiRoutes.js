2802bd4251d31f43a3c9d3f8de7ec03b72e8eda3
Sam Ayler <sayler8701@gmail.com>
2021-02-08 16:05:33 -0800

    setting up routes to add workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const Workout = require("../models/workout");
 module.exports = (app) => {
 	//find all workouts
 	app.get("/api/workouts", (req, res) => {
-		Workout.find()
+		Workout.find({})
 			.then((found) => {
 				res.json(found);
 			})
@@ -12,10 +12,29 @@ module.exports = (app) => {
 			});
 	});
 
-	//create a workout
-	// app.post("/api/workouts", (req, res) => {
-	// 	Workout.create({
-	// 		date: Date.now(),
-	// 	});
-	// });
+	// create a workout
+	app.post("/api/workouts", (req, res) => {
+		Workout.create(req.body)
+			.then((data) => {
+				res.json(data);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	});
+
+	// create a excercise
+	app.put("/api/workouts/:id", (req, res) => {
+		Workout.findOneAndUpdate(
+			{ _id: req.params.id },
+			{ $push: { exercises: req.body } },
+			{ new: true }
+		)
+			.then((data) => {
+				res.json(data);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	});
 };
